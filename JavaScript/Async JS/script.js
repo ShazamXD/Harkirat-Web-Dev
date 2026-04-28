952b83ea89707ccdff9e9a37a4c480d869f139e8
Chitransh Pal <chitransh.pal108@gmail.com>
2026-04-28 10:19:40 +0530

    Enhance script.js with detailed examples for synchronous code and I/O heavy operations

--- JavaScript/Async JS/script.js
+++ JavaScript/Async JS/script.js
@@ -4,12 +4,38 @@
 executed line by line, in the order it is written. Each operation waits for the
 previous one to complete before moving to the next one
 
+example: function sum(n) {
+	    let ans = 0;
+	    for (let i = 1; i <= n; i++) {
+		ans = ans + i
+	    }
+	    return ans;
+    }
+        const ans1 = sum(100);
+        console.log(ans1);
+        const ans2 = sum(1000);
+        console.log(ans2);
+        const ans3 = sum(10000);
+        console.log(ans3);
 
 
+-- I/O HEAVY OPERATIONS --
+These operations usually require waiting for data to be read from or written to 
+sources like disks, networks, databases, or other external devices, which can be 
+time-consuming compared to in-memory computations.
 
+example:
+        const fs = require("fs"); // when you have to read the data from external file
+                                     you use this to import it into js first 
 
+const contents = fs.readFileSync("a.txt", "utf-8"); // after import it works as a
+                  object and you can access the keys just like in normal js objects
+console.log(contents);
 
+const contents2 = fs.readFileSync("b.txt", "utf-8"); // here b.txt is the path and 
+                                                        utf-8 is the encrypt form of data
+console.log(contents2);
 
-
+// this code reads the other file synchronously
 
 */
